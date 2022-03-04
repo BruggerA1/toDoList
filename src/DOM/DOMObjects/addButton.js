a079ac3994f6ca90f7343b75f8119de3ec34b895
Brugger Anderson <BruggerA1@gmail.com>
2022-03-04 11:02:24 -0500

    *Add

--- src/DOM/DOMObjects/addButton.js
+++ src/DOM/DOMObjects/addButton.js
@@ -2,7 +2,6 @@ import { UIelement } from './UIelement';
 
 export const AddButton = (label) => {
 	const addButton = UIelement('button', `add${label}Button`);
-
 	addButton.addClass('addButton');
 	addButton.UItext(`+ ${label}`);
 
