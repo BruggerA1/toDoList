3d86e744f07cc0dff62c9e80d92b2bfe03ff8f75
Brugger Anderson <BruggerA1@gmail.com>
2022-04-07 08:33:40 -0400

    Test Functions

--- src/DOM/DOMObjects/SidebarCategory.js
+++ src/DOM/DOMObjects/SidebarCategory.js
@@ -1,5 +1,6 @@
 import { ui } from "../..";
 import { CardElement } from "./CardElement";
+import { ItemCard } from "./ItemCard";
 
 export const SidebarCategory = (categoryText) => {
 	const sidebarCategory = CardElement('div', 'sidebarCategory');
@@ -13,6 +14,41 @@ export const SidebarCategory = (categoryText) => {
 			: (categoryText == 'Today') ? cardLabel.UItext(projectList.getTodayItems().length)
 				: cardLabel.UItext(projectList.getWeekItems().length);
 	}
+
+	//test
+	const loadAllProjects = () => {
+		const projectList = ui.sidebar.projectContainer.projectList;
+		content.reload();
+
+		content.currentProject.UItext(categoryText);
+
+		projectList.getItems().forEach(item => content.append(ItemCard(item)));
+	};
+
+	const loadTodayProjects = () => {
+		const projectList = ui.sidebar.projectContainer.projectList;
+		content.reload();
+
+		content.currentProject.UItext(categoryText);
+
+		projectList.getTodayItems().forEach(item => content.append(ItemCard(item)));
+	};
+
+	const loadWeekProjects = () => {
+		const projectList = ui.sidebar.projectContainer.projectList;
+		content.reload();
+
+		content.currentProject.UItext(categoryText);
+
+		projectList.getWeekItems().forEach(item => content.append(ItemCard(item)));
+	};
+
+	sidebarCategory.addEventListener('click', () => {
+		const projectList = ui.sidebar.projectContainer.projectList;
+		(categoryText == 'All') ? loadAllProjects()
+			: (categoryText == 'Today') ? loadTodayProjects()
+				: loadWeekProjects();
+	})
 
 	sidebarCategory.append(cardText, cardLabel);
 
